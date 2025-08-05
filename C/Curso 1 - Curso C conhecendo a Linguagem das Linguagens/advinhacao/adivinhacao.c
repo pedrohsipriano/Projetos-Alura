eb607a9b6a6db71dd8915eadd15437693abb65a3
Pedro Henrique <pedrohsipriano00@gmail.com>
2025-08-05 16:43:27 -0300

    continuação curso c

--- C/Curso 1 - Curso C conhecendo a Linguagem das Linguagens/advinhacao/adivinhacao.c
+++ C/Curso 1 - Curso C conhecendo a Linguagem das Linguagens/advinhacao/adivinhacao.c
@@ -1,32 +1,63 @@
 #include <stdio.h>
 #include <locale.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main() {
+#define NUMERO_DE_TENTATIVAS 5
+
+int main()
+{
     setlocale(LC_ALL, "Portuguese");
     system("cls");
 
-    //imprime uma mensagem de boas-vindas
+    // imprime uma mensagem de boas-vindas
     printf("******************************************\n");
-    printf("* Bem-vindo ao nosso jogo de adivinha??o *\n");
+    printf("* Bem-vindo ao nosso jogo de adivinhação *\n");
     printf("******************************************\n");
-    
-    int numerosecreto = 42;
-    printf("O número secreto é: %d\n", numerosecreto);
 
-    int chute;
-    printf("Qual é o seu chute? ");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d\n", chute);
+    int segundos = time(0);
+    srand(segundos);
+    int numerosecreto = segundos % 100; // Gera um número secreto entre 0 e 99
+    printf("O número secreto é: %d\n", numerosecreto);
+    int chute = 0;
+    int contador = 1;
+    double pontos = 1000;
     
-    if (chute == numerosecreto){
-        printf("Parabéns! você acertou!\n");
-    }else if(chute < numerosecreto){
-        printf("Você errou! O número secreto é maior.\n");
-    }else if(chute > numerosecreto){
-        printf("Você errou! O número secreto é menor.\n");
-    }else{
-        printf("Você errou! Tente novamente.\n");
-    }
+    do
+    {
+        printf("\nTentativa %d\n", contador);
+        printf("Qual é o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi %d\n", chute);
+
+        if (chute < 0)
+        {
+            printf("Por favor, insira um número positivo.\n");
+            continue;
+        }
+        
+
+        if (chute == numerosecreto)
+        {
+            printf("Parabéns! você acertou!\n");
+            break;
+        }
+        else if (chute < numerosecreto)
+        {
+            printf("Você errou! O número secreto é maior.\n");
+        }
+        else if (chute > numerosecreto)
+        {
+            printf("Você errou! O número secreto é menor.\n");
+        }
+        contador++;
+
+        double pontosPerdidos = abs(chute - numerosecreto) / 2.0;
+        pontos -= pontosPerdidos;
+    } while (1);
+
+    printf("Fim do jogo!\n");
+    printf("Você acertou o número secreto em %d tentativas!\n", contador);
+    printf("Sua pontuação final é: %.2f\n", pontos);
     return 0;
 }
---
+++ C/Curso 2 - C avançando na linguagem/forca.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    system("cls");
+    return 0;
+}
